aa433f60ca6a8a4c7c021865f29d96cea1a0dc55
Hassan-Adelani-luqman <h.luqman@alustudent.com>
2026-03-11 19:51:26 +0200

    fix: register health router and request logger in app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 
+const requestLogger = require('./middleware/logger');
 const tasksRouter = require('./routes/tasks');
+const healthRouter = require('./routes/health');
 
 app.use(express.json());
+app.use(requestLogger);
 
 app.use('/tasks', tasksRouter);
+app.use('/health', healthRouter);
 
 module.exports = app;
